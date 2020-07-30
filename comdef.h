ceabb7369c7855ac8295fca460a67e92a0dfdfcc
BLINA4 <shayda.andrey@yandex.ru>
2020-07-30 18:49:46 +0300

    Initial type redeclaration file + main function dummy

---
+++ comdef.h
@@ -0,0 +1,39 @@
+/***************************************************************
+ * Copyleft 2020
+ *   Junior C programmer presents (lol)
+ ***************************************************************/
+
+/* FILE NAME   : comdef.h
+ * PURPOSE     : 2D game project.
+ *               Common defines of project
+ * PROGRAMMER  : Andrey Shayda.
+ * LAST UPDATE : 22.03.2020.
+ *
+ * All parts of this file may be changed without agreement
+ *   of programmer if you give credits to author.
+ */
+
+#ifndef __comdef_h_
+#define __comdef_h_
+
+/* main project headers */
+#include <GL/gl.h>
+#include <GL/glx.h>
+#include <GL/glu.h>
+
+/* redefining main C types */
+typedef void VOID;
+typedef int INT;
+typedef unsigned int UINT;
+typedef float FLT;
+typedef double DBL;
+typedef char CHAR;
+typedef unsigned char BYTE;
+typedef short SHORT;
+typedef unsigned short USHORT;
+typedef long LONG;
+typedef unsigned long ULONG;
+
+#endif /* __comdef_h_ */
+
+/* END OF 'comdef.h' FILE */
--- main.c
+++ main.c
@@ -1,1 +1,26 @@
-/* Initial Commit */
+ /***************************************************************
+  * Copyleft 2020
+  *   Junior C programmers present
+  ***************************************************************/
+ 
+ /* FILE NAME   : main.c
+  * PURPOSE     : 2D game project.
+  *               Main file of project.
+  * PROGRAMMER  : Andrey Shayda, Petr Petrovich, Ignat Krisow.
+  * LAST UPDATE : 30.07.2020.
+  *
+  * All parts of this file may be changed without agreement
+  *   of programmer if you give credits to author.
+  */
+
+/* Main programm function.
+ * ARGUMENTS: None.
+ * RETURN: None.
+ */
+void main( void )
+{
+
+} /* End of 'main' function */
+
+/* END OF FILE 'main.c' */
+
